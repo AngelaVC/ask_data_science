7f14f6dda4c3ff68f295bf4dc143966b85c60286
Angela V-C <angela.the.vc@gmail.com>
2017-08-10 09:44:42 -0400

    Adding getTop

--- scrape/web.py
+++ scrape/web.py
@@ -1,12 +1,57 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
+import re
 
+def find_top_content(soup):
+    '''This script is somewhat inspired by things
+        at http://nirmalpatel.com/fcgi/hn.py'''
+    neg_class = re.compile("comment|meta|footer|footnote|foot")
+    pos_class = re.compile("post|hentry|entry|content|text|body|article")
+
+    paragraphs = soup.findAll("p")
+
+    top_parent = None
+    parents = []
+
+    for paragraph in paragraphs:
+        parent = paragraph.parent
+
+        if (parent not in parents):
+            parents.append(parent)
+            parent.score = 0
+
+        # look at presence of positive or negative words in
+        # class or id attributes
+        if (parent.has_attr("class")):
+
+            if any([bool(
+                    neg_class.search(cls)) for cls in parent['class']]):
+                parent.score -= 1
+
+            if any([bool(
+                    pos_class.search(cls)) for cls in parent['class']]):
+                parent.score += 1
+
+        if (parent.has_attr("id")):
+
+            if any([bool(neg_class.search(cls)) for cls in parent['id']]):
+                parent.score -= 1
+
+            if any([bool(pos_class.search(cls)) for cls in parent['id']]):
+                parent.score += 1
+
+    for parent in parents:
+        print(parent.score)
+        if parent.score > top_parent.score:
+            top_parent = parent
+    return top_parent
 
 class WebPage:
     def __init__(self, url=None):
         self.url = url
         self.links = []
         self.soup = None
+        self.top = None
 
     def getSoup(self):
 
@@ -19,19 +64,25 @@ class WebPage:
 
     def getLinks(self):
         '''This gets links starting with 'http' from single page.
+        Just the links that are in the top of the page
         '''
 
         # XXXX what if there are no 'a' tags?
 
-        for link in self.soup.findAll('a'):
+        for link in self.top.findAll('a'):
             if ('href' in link.attrs) & (
                         link.attrs['href'].startswith("http")):
                 self.links.append(link.attrs['href'])
         return self.links
 
+    def getTop(self):
+        # get the parent that holds the important content
+        self.top = find_top_content(self.soup)
+        return self.top
+
     def getText(self):
-        # kill all script, style, tables
-        for junk in self.soup(["script", "style", "table"]):
+        # kill all script, style
+        for junk in self.soup(["script", "style"]):
             junk.decompose()
 
 
