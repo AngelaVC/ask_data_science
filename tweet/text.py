99c7bc9a532f5c63995aa6dbdc6b3c28975848be
Angela V-C <angela.the.vc@gmail.com>
2017-08-17 17:45:01 -0400

    added words and trigrams

--- tweet/text.py
+++ tweet/text.py
@@ -1,20 +1,12 @@
-from scrape import web
-from urllib.request import urlopen
-from tinydb import TinyDB, Query
-
 import re
-
-from collections import defaultdict
-
-import random
 
 
 def clean_text(text):
-    text = re.sub("’","'",text)
-    text = re.sub(r"’","'",text) # single quotes replace with apostrophe
-    text = re.sub(r"\b[0-9]+.\b"," ",text)  #numbers only
-    text = re.sub(r"\b\w+@\w+.\w+\b"," ",text) #email addresses
-    text = " ".join(text.split()) # gets rid of multi spaces, \r etc
+    # cleans various text components
+    text = re.sub("\u2019", "'", text)  # replace single quote with apostr
+    text = re.sub(r"\b[0-9]+.\b", " ", text)  # numbers only
+    text = re.sub(r"\b\w+@\w+.\w+\b", " ", text)  # email addresses
+    text = " ".join(text.split())  # gets rid of multi spaces, \r etc
 
     return text
 
@@ -22,7 +14,35 @@ def clean_text(text):
 class Generated():
     ''' Class for generated tweet. Could include a topic, text, image, link.
     First just building the text part.
+    Generated tweet based on text in db, which has 'text' field
     '''
-    def __init__(self, db):
-        self.topic = None
+    def __init__(self, db=None, topic=None):
+        self.db = db
+        self.topic = topic
         self.text = None
+        self.words = []
+        self.trigrams = None
+
+    def gatherText(self):
+        ''' Takes a TinyDB with a "text" field and combines all the text into
+        a one list of texts
+        '''
+        all_text = ""
+        for item in self.db:
+            all_text + " " + clean_text(item['text'])
+        self.text = all_text
+        return self.text
+
+    def getWords(self):
+        self.words = re.findall(r"[\w']+|[.!?;,]", self.all_text)
+        return self.words
+
+    def getTrigrams(self):
+        '''Creates trigrams from words. If words has not yet been created
+        executes getWords
+        '''
+        if len(self.words) == 0:
+            self.getWords()
+
+        self.trigrams = zip(self.words, self.words[1:], self.words[2:])
+        return self.trigrams
