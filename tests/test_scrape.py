775b3dc0e015aeebcc4b412d72a8f33338bffcfc
Angela V-C <angela.the.vc@gmail.com>
2017-08-18 11:31:20 -0400

    test WebPage

--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -9,3 +9,13 @@ This script tests methods a webscraping class called WebPage
 def webpage():
     from scrape.web import WebPage
     return WebPage()
+
+
+test_site_data = [
+                    ('http://google.com', True),
+                    ('htt:google.com', True)]
+
+
+@pytest.mark.parametrize('x, expected', test_site_data)
+def test_perfect_number(x, expected, webpage):
+    assert webpage.html == expected
