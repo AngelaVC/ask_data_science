1f3a06f941cc7f9a039adb2aec97d94a8995efe2
Angela V-C <angela.the.vc@gmail.com>
2017-08-18 12:02:18 -0400

    initial test

--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -1,21 +1,21 @@
 import pytest
 
 """
-This script tests methods a webscraping class called WebPage
+This script tests a webscraping class called WebPage
 """
 
-
+# TODO still need to fix this first test
 @pytest.fixture
-def webpage():
-    from scrape.web import WebPage
-    return WebPage()
+def webpage(url):
+    from scrape.web import WebPage, database
+    return WebPage(url)
 
 
 test_site_data = [
                     ('http://google.com', True),
-                    ('htt:google.com', True)]
+                    ('htt:google.com', 'Error')]
 
 
 @pytest.mark.parametrize('x, expected', test_site_data)
-def test_perfect_number(x, expected, webpage):
+def test_webpage(x, expected, webpage):
     assert webpage.html == expected
