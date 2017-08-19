4eda8b1d16c205e68407701fe0584beb229d3365
Angela V-C <angela.the.vc@gmail.com>
2017-08-18 21:02:45 -0400

    fix Generated()

--- tweet/text.py
+++ tweet/text.py
@@ -3,6 +3,7 @@ from tinydb import TinyDB, Query
 from collections import defaultdict
 import random
 
+enders = ['.', '!', '?', ';', ',', ':']
 
 def clean_text(text):
     # cleans various text components
@@ -32,7 +33,11 @@ class Generated():
         self.gatherText()
         self.getWords()
         self.getTrigrams()
-        self.getTransitions()
+        self.getTransitions(enders)
+
+    def debug(self):
+        for item in self.db:
+            print(item['text'])
 
     def gatherText(self):
         ''' Takes a TinyDB with a "text" field and combines all the text into
@@ -40,11 +45,11 @@ class Generated():
         '''
         all_text = ""
         for item in self.db:
-            all_text + " " + clean_text(item['text'])
+            all_text = all_text + " " + clean_text(item['text'])
         self.text = all_text
 
     def getWords(self):
-        self.words = re.findall(r"[\w']+|[.!?;,]", self.text)
+        self.words = re.findall(r"[\w']+|[.!?;,:]", self.text)
 
     def getTrigrams(self):
         '''Creates trigrams from words. If words has not yet been created
@@ -55,25 +60,52 @@ class Generated():
 
         self.trigrams = zip(self.words, self.words[1:], self.words[2:])
 
-    def getTransitions(self):
+    def getTransitions(self, snt_end=enders):
         transitions = defaultdict(list)
         for one, two, three in self.trigrams:
-            transitions[one].append([two, three])
+            if (two not in snt_end) & (three not in snt_end):
+                transitions[one].append([two, three])
         self.transitions = transitions
 
-    def writeTweet(self):
-        print(self.transitions)
-        snt_end = ['.', '!', '?', ';']
-        next_start = random.choice(self.transitions['.'])
-        sentance = next_start[0]
+    def getRandomLink(self):
+        links = []
+        for item in self.db:
+            links.extend(item['links'])
+        return random.choice(links)
+
+    def writeTweet(self, snt_end=enders, min_length=10):
+        ''' This function starts with a random choice anything after a
+            sentance ender as the start of a tweet. Tweet builds from there
+            using trigrams. If tweet is too long, it is split into parts.
+            If there is space, a link is enclosed.
+        '''
+        next_start = random.choice(random.choice(
+                                [self.transitions[end] for end in snt_end]))
+        tweet = next_start[0]
         next_word = next_start[1]
-        while (next_word not in snt_end) & (next_word is not None):
-            sentance = sentance + ' ' + next_word
-            next_start = random.choice(self.transitions[next_word])
-            if next_start[0] in snt_end:
-                return sentance + ' ' + next_start[0]
+        while (len(tweet) + len(next_word) < 135):
+            if next_word in snt_end:
+                if len(tweet) > min_length:
+                    if len(tweet) < 140-23:
+                        tweet = tweet + self.getRandomLink()
+                    else:
+                        tweet = tweet + random.choice(['.', '.', '!', '?'])
+                    return tweet
+                else:
+                    tweet = tweet + random.choice(['.', '.', '!', '?'])
             else:
-                sentance = sentance + ' ' + next_start[0]
+                tweet = tweet + ' ' + next_word
+
+            if self.transitions[next_word] == []:
+                next_start = random.choice(self.transitions[
+                                                random.choice(self.words)])
+            else:
+                next_start = random.choice(self.transitions[next_word])
+
+            if next_start[0] in snt_end:
+                return tweet + random.choice(['.', '.', '!', '?'])
+            else:
+                tweet = tweet + ' ' + next_start[0]
                 next_word = next_start[1]
 
-        return sentance
+        return tweet
