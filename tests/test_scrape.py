883b718bd542d0afd666e70620610b0e8265f256
Angela V-C <angela.the.vc@gmail.com>
2017-08-22 18:09:51 -0400

    two unit tests

--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -1,21 +1,41 @@
 import pytest
+from tinydb import TinyDB
 
-"""
-This script tests a webscraping class called WebPage
-"""
+test_db = TinyDB('test_db.json')
+test_db.insert({
+        'url': 'http://google.com',
+        'title': 'A Title',
+        'text': "The best text. Even best stuff.",
+        'links': ['http://link1.html', 'http://link2.html']})
 
-# TODO still need to fix this first test
+
 @pytest.fixture
-def webpage(url):
-    from scrape.web import WebPage, database
-    return WebPage(url)
+def gen():
+    from ..tweet.generate import Generated
+    generated = Generated('test_db.json')
+    generated.getAll()
+    return generated
 
 
-test_site_data = [
-                    ('http://google.com', True),
-                    ('htt:google.com', 'Error')]
+test_data = [((test_db, 'The'), ('The', 'best')), ((test_db, 'XX124'), (None, None))]
+@pytest.mark.parametrize('x,expected',test_data)
+def test_firstWords(x, expected, gen):
+    # Test initialization, not sure how
+    # assert isinstance(gen(input1[0]), Generated)
+
+    tweet, next_word = gen.firstWords()
+    assert str(tweet == expected[0])
+    assert str(next_word == expected[1])
 
 
-@pytest.mark.parametrize('x, expected', test_site_data)
-def test_webpage(x, expected, webpage):
-    assert webpage.html == expected
+test2_data = [((test_db, 'In the dark', 'text'), ('In the dark text', '.')),
+              ((test_db, 'One or two', 'The'), ('One or two The', 'best'))]
+@pytest.mark.parametrize('x,expected', test2_data)
+def test_nextWords(x, expected, gen):
+
+    tweet, next_word = gen.nextWords(x[1
+
+    assert str(tweet == expected[0])
+    assert str(next_word == expected[1])
+
+#test_generated_class(input1, expected1)
