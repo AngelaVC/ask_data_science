51be1542fcda57ee283f25bdbb863044f2d11501
Angela V-C <angela.the.vc@gmail.com>
2017-08-30 15:46:11 -0400

    Adjust nounReplyStarter, add comments

--- tweet/reader.py
+++ tweet/reader.py
@@ -39,22 +39,36 @@ class readTweet():
             self.nounFreq[noun] = self.generator.wordFreq[noun]
 
     def bigramReplyStarter(self):
+        # This takes the bigram that occurs least frequently in all of the
+        # text bigram and uses it to start a tweet
+        # TODO need to take out any sentance enders (so that they don't appear
+        # in tweet starter
         best_count = 1000000
         for key in self.bigramFreq:
-            if (self.bigramFreq[key] > 0) & (self.bigramFreq[key] < best_count):
+            if (self.bigramFreq[key] > 0) & (self.big.ramFreq[key] < best_count):
                 self.replyStart = key[0] + ' ' + key[1]
                 best_count = self.bigramFreq[key]
         if best_count == 1000000:
             self.replyStart = "What are you talking about?"
 
     def nounReplyStarter(self):
+        # This takes the least frequent noun and uses it to start a reply
         best_count = 1000000
         for key in self.nounFreq:
             if (self.nounFreq[key] > 0) & (self.nounFreq[key] < best_count):
                 next_start = random.choice(self.generator.transitions[key])
-                while (len(set(next_start).intersection(set(self.generator.enders))) > 0):
+                # need to reselect if next_start contains a sentance ender
+                # will escape after 25 tries
+                try_count = 0
+                while (len(set(next_start).intersection(
+                       set(self.generator.enders))) > 0) & try_count < 25:
                     next_start = random.choice(self.generator.transitions[key])
+                    try_count += 1
+
+                # update replyStart and the best_count before going to next
                 self.replyStart = key.capitalize() + ' ' + ' '.join(next_start)
                 best_count = self.nounFreq[key]
+
         if best_count == 1000000:
+            # this happens if none of the keys
             self.replyStart = "What are you talking about? "
